4d23110cc176b1177bf8dfad935f4de5e2e711c2
Zakir Ayub <ayubzakirtushar@gmail.com>
2022-03-19 15:02:25 +0100

    updated for garbe

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from selenium.webdriver.common.by import By
 import time
 import random
 
-
-
 chrome_options = Options()
 chrome_options.headless = False
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -15,7 +13,9 @@ browser = webdriver.Chrome(options=chrome_options)
 def garbe():
     url = "https://www.garbe-immobilien-projekte.de/projekte/"
     browser.get(url)
-    project_name_link = browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/figure/a")
+    project_name_link = browser.find_element(by=By.XPATH,
+                                             value="/html/body/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/figure/a")
     project_name_link.click()
+
 
 garbe()
