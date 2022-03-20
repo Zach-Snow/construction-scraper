be6c6d2510ac22660604bcf4ef30e52f23c9c181
Zakir Ayub <ayubzakirtushar@gmail.com>
2022-03-20 23:29:56 +0100

    project_dict module added

--- project_dict.py
+++ project_dict.py
@@ -1,6 +1,6 @@
 project_dictionary = {"project_name": "",
                       "project_location": "",
-                      "customer": "GARBE Immobilien-Projekte",
+                      "customer": "",
                       "project_information": "",
                       "scraping_date": "",
                       "link": ""}
