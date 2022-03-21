0b90a52802d3b76483c6b11b0354472296ca9490
Zakir Ayub <ayubzakirtushar@gmail.com>
2022-03-21 14:29:07 +0100

    added favicon and templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory, render_template
 from flask_restful import Api
 from garbe_scraper import garbe_scraper
 from time import sleep
@@ -9,6 +9,17 @@ from brandberger_scraper import brandberger_scraper
 
 app = Flask(__name__)
 api = Api(app)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 @app.route('/garbe_new', methods=['GET'])
